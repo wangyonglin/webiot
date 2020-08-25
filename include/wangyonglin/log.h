96f2e4147774f579ceb97f5e7e54913773bcd5e7
root <root@localhost.localdomain>
2020-08-26 02:27:29 +0800

    OK

--- include/wangyonglin/log.h
+++ include/wangyonglin/log.h
@@ -7,12 +7,14 @@
 
 
 struct wangyonglin_log_s{
+	wangyonglin_uint_t log_level;
+	FILE * fd;
 	char * msg;
 };
 
 
-
-
+void wangyonglin_file_fopen(wangyonglin_string_t file,wangyonglin_log_t * log);
+void wangyonglin_log_error(wangyonglin_uint_t level,wangyonglin_log_t * log,const char *fmt, ...);
 
 
 #endif /**_WANGYONGLIN_LOG_H_INCLUDE_**/
--- src/wangyonglin/log.c
+++ src/wangyonglin/log.c
@@ -14,5 +14,27 @@ static wangyonglin_string_t err_levels[] = {
 	    wangyonglin_string("debug")
 	};
 
+void wangyonglin_file_fopen(wangyonglin_string_t file,wangyonglin_log_t * log){
+		if( (log->fd=fopen(file->data,"rb") == NULL )){
+			fprintf(stderr,"Fail to open file!\n");
+			exit(0);  //退出程序（结束程序）
+		}
+}
+void wangyonglin_file_fclose(wangyonglin_log_t * log){
 
-
+	if(log->fd!=NULL){
+		fclose(log->fd);
+	}
+}
+void wangyonglin_log_error(wangyonglin_uint_t level,wangyonglin_log_t * log,const char *fmt, ...){
+		wangyonglin_file_fopen(wangyonglin_string("/usr/local/nginx/log/error.log"),log);
+		va_list  args;
+	    if (log->log_level >= level) {
+	        va_start(args, fmt);
+	       if(log->fd!=NULL){
+			   fprintf(log->fd,fmt,args);
+		   }
+	        va_end(args);
+	    }
+		wangyonglin_file_fclose(log);
+}
